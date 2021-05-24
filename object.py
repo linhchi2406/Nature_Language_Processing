ccc4bfee451850d3256b4b0d30a7ad835341220e
linhchi2406 <linhchi24060508@gmail.com>
2021-05-24 21:53:22 +0700

    Connect neo4j

--- object.py
+++ object.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # import underthesea
 import re
+import neo4j_until as neo4j
 import mysql.connector
-# from underthesea import ner
 
 f = open('out.txt', 'w+', encoding='utf-8')
+
 
 FEMALE = [r"(n|N)ữ"]
 MALE = [r"(n|N)am", "nam giới"]
@@ -75,7 +76,7 @@ def BNrange(text):
             return(ids)
             break
         return None
-# lấy id của các của bệnh nhân
+
 def getBNid(text):
     BNs=[]
     for i in BNre:
@@ -86,7 +87,7 @@ def getBNid(text):
                 BNs.append(re.findall(r"[0-9]{1,4}", bn)[0])
     return ["BN"+BNid for BNid in BNs]
 
-# chuyển file text thành định dạng BN và thành phố
+
 def preprocessIDBN(text):
     BNs=[]
     for i in BNre:
@@ -99,17 +100,16 @@ def preprocessIDBN(text):
     text = text.replace("TP. ", "thành phố ")
     return text
 
-# tách đoạn văn thành các đoạn sau dấu chấm và dấu chấm phẩy
+
 def seperateSentences(text):
     sentences = []
     for sentence in text.split('.'):
         sentences += sentence.split(";")
     return sentences
-#Lấy mối quan hệ của các bệnh nhân
 def getRelation(text):
     BNids = getBNid(text)
-    # for BNid in BNids:
-        # neo4j.createBN(BNid,None, None, None, None, None, None, None, None)
+    for BNid in BNids:
+        neo4j.createBN(BNid,None, None, None, None, None, None, None, None)
     if len(BNids) < 2:
         return
     BNid_main = BNids[0]
@@ -140,12 +140,11 @@ def getRelation(text):
                     if relation == None:
                         relation = sub
                     print("Relation:",BNid1, relation, BNid2)
+                    neo4j.createConnect(BNid1, relation, BNid2)
                     f.write("\nRelation: "+BNid1)
                     f.write(relation)
                     f.write(BNid2)
-                    # neo4j.createConnect(BNid1, relation, BNid2)
 
-#Lấy quốc tịch của bệnh nhân
 def getNationlaty(text):
     for i in NATIONLATY_RE:
         result = re.search(i, text, flags)
@@ -155,27 +154,26 @@ def getNationlaty(text):
                 return match_obj_country.group(0)
             else:
                 return result.group(0)
-#Lấy thông tin về địa chỉ của bệnh nhân
+
 def getOrigin(text):
     for i in ORIGIN:
         result = re.search(i, text, flags)
         if result:
             return result.group(0)
-#Thông tin về chuyến bay
+
 def getFlight(text):
     for i in FLIGHT_RE:
         result = re.search(i, text)
         if result:
             return result.group(0)
     return None
-#Thông tin về số ghế
 def getNumberSit(text):
     for i in NUMBERSIT:
         result = re.search(i, text)
         if result:
             return result.group(0)
     return None
-#Tách các thực thể 
+
 def process(text, date=None):
     BNid_main = None
     for sentence in seperateSentences(text):
@@ -184,82 +182,62 @@ def process(text, date=None):
         print("#"*32)
         f.write("\nSentences: "+sentence)
         print("Sentences:",sentence)
-        #Lấy id của bệnh nhân
         BNids = getBNid(sentence)
         if len(BNids) != 0:
             BNid_main = BNids[0]
-            # in ra mã bệnh nhân
-        if BNid_main:
-            f.write("\n"+BNid_main)
-            print(BNid_main)
+        print(BNid_main)
         if date:
-            f.write("\n")
-            f.write(date)
-            # neo4j.updateBN(BNid_main, "date", date)
+            neo4j.updateBN(BNid_main, "date", date)
+            f.write("\n"+BNid_main)
         sex = getSex(sentence)
         if sex != None:
+            neo4j.updateBN(BNid_main, "sex", sex)
             f.write("\nSex:"+sex)
-
-            # neo4j.updateBN(BNid_main, "sex", sex)
-            print("Sex:",sex)
         age = getAge(sentence)
         if age != None:
-            # neo4j.updateBN(BNid_main, "age", age)
+            neo4j.updateBN(BNid_main, "age", age)
             f.write("\n Age:" +age)
-            print("Age:",age)
         flight = getFlight(sentence)
         if flight != None:
+            neo4j.createTranspotation(BNid_main, flight)
             f.write("\n Flight:"+flight)
-            # neo4j.createTranspotation(BNid_main, flight)
-            print("Flight:",flight)
             numbersit = getNumberSit(sentence)
             if numbersit != None:
+                neo4j.updateBN(BNid_main, "number_sit", numbersit)
                 f.write("\n Numbersit:"+numbersit)
-                # neo4j.updateBN(BNid_main, "number_sit", numbersit)
-                print("Numbersit:",numbersit)
-                # neo4j.createConnectPTVT(BNid_main, numbersit, flight)
+                neo4j.createConnectPTVT(BNid_main, numbersit, flight)
         nationlaty = getNationlaty(sentence)
         if nationlaty != None:
+            neo4j.updateBN(BNid_main, "nationlaty", nationlaty)
             f.write("\n Nation:"+nationlaty)
-            # neo4j.updateBN(BNid_main, "nationlaty", nationlaty)
-            print("Nation:",nationlaty)
         origin = getOrigin(sentence)
         if origin != None:
+            neo4j.updateBN(BNid_main, "origin", origin)
             f.write("\nOrigin:"+origin)
-            # neo4j.updateBN(BNid_main, "origin", origin)
-            print("Origin:",origin)
         status = getStatus(sentence)
         if status != None:
+            neo4j.updateBN(BNid_main, "status", status)
             f.write("\n Status: "+status)
-            # neo4j.updateBN(BNid_main, "status", status)
-            print("Status:",status)
 
 def getObject(text, date=None):
     try:
-        #chuyển về đúng định dạng câu văn
         text = preprocessIDBN(text)
-        #Xem xét các quan hệ của bệnh nhân
-        # getRelation(text)
-        #Lấy các thông tin của bệnh nhân
+        getRelation(text)
         process(text, date)
     except Exception as e:
         print("Error: ",e)
 
 if __name__ == '__main__':
-#     text = """ 
- 
-# - CA BỆNH 2825  ghi nhận tại Thành phố Đà Nẵng: Bệnh nhân nam, 37 tuổi, công dân VN, địa chỉ tại quận Hoàng Mai, Thành phố Hà Nội. Ngày 08/4/2021, bệnh nhân từ Nhật Bản nhập cảnh Sân bay Quốc tế Đà Nẵng trên chuyến bay VJ8679 và được cách ly ngay sau khi nhập cảnh tại Thành phố Đà Nẵng. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện Phổi Đà Nẵng.
- 
-# - CA BỆNH 2826 (BN2826) ghi nhận tại tỉnh Bà Rịa - Vũng Tàu: Bệnh nhân nam, 50 tuổi, quốc tịch Philippnes, là thuyền viên trên tàu di chuyển từ Trung Quốc đến tỉnh Bà Rịa - Vũng Tàu ngày 18/4/2021. Đây là trường hợp được cách ly ngay trên tàu tại khu vực neo đậu Cảng vụ hàng hải Vũng Tàu. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Trung tâm Y tế huyện Long Điền.
- 
-# - CA BỆNH 2827 (BN2827) ghi nhận tại tỉnh Khánh Hòa: Bệnh nhân nam, 37 tuổi, quốc tịch Ấn Độ, là chuyên gia. Bệnh nhân từ Ấn Độ quá cảnh Dubai, Hàn Quốc, sau đó nhập cảnh Sân bay Cam Ranh trên chuyến bay OZ773 ngày 20/4/2021 và được cách ly ngay sau khi nhập cảnh tại tỉnh Khánh Hòa. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện Bệnh nhiệt đới tỉnh Khánh Hòa.
- 
-# - CA BỆNH 2828 (BN2828) ghi nhận tại tỉnh An Giang: Bệnh nhân nam, 23 tuổi, công dân Việt Nam, địa chỉ tại huyện An Phú, tỉnh An giang. Ngày 20/4/2021 bệnh nhân trên từ nước ngoài nhập cảnh tại Cửa khẩu Long Bình và được cách ly ngay sau khi nhập cảnh tại tỉnh An Giang. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Trung tâm y tế huyện An Phú, tỉnh An Giang.
- 
-# - CA BỆNH 2829 (BN2829) ghi nhận tại tỉnh Khánh Hòa: Bệnh nhân nữ, 23 tuổi, công dân Việt Nam, địa chỉ tại huyện Long Điền, tỉnh Bà Rịa - Vũng Tàu. Bệnh nhân từ Nga nhập cảnh Sân bay Cam Ranh trên chuyến bay VN62 ngày 18/4/2021 và được cách ly ngay sau khi nhập cảnh tại tỉnh Khánh Hòa. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện Bệnh nhiệt đới tỉnh Khánh Hòa.
- 
-# - CA BỆNH 2830 (BN2830) ghi nhận tại Thành phố Hồ Chí Minh: Bệnh nhân nữ, 27 tuổi, công dân Việt Nam, địa chỉ tại quận Hoàn Kiếm, Thành phố Hà Nội. Ngày 21/4/2021, bệnh nhân từ Nhật Bản nhập cảnh Sân bay Tân Sơn Nhất trên chuyến bay JL759 và được cách ly ngay sau khi nhập cảnh tại Thành phố Hồ Chí Minh. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện Dã chiến Củ Chi.
-# """
+    # text = """THÔNG BÁO 7 CA BỆNH MỚI SỐ 107-113: BN107: nữ, 25 tuổi,đã chết, quốc tịch Việt Nam, nhân viên thiết kế đồ họa, là con gái và sống cùng BN86.
+    # Có địa chỉ thường trú tại Thanh Xuân, Hà Nội; BN108: nam, 19 tuổi, quốc tịch Việt Nam, địa chỉ ở Cầu Giấy, Hà Nội.
+    #     Bệnh nhân là du học sinh Việt Nam tại Anh về nước ngày 18/3 trên chuyến bay VN054; BN109: nam, 42 tuổi, quốc tịch Việt Nam, địa chỉ ở Hoàng Mai, Hà Nội.
+    #         Bệnh nhân là giảng viên một trường đại học của Anh, về nước ngày 15/3/2020 trên chuyến bay TG 560, số ghế 37E; BN110: nữ, 19 tuổi, quốc tịch Việt Nam, địa chỉ ở Đống Đa, Hà Nội.
+    #             Bệnh nhân là du học sinh tại Mỹ, hy sinh tạitại Việt Nam ngày 19/03/2020 trên chuyến bay JL751, số ghế 1A; BN111: nữ, 25 tuổi, quốc tịch Việt Nam, địa chỉ ở Hải Hậu, Nam Định.
+    #                 Bệnh nhân là du học sinh tại Pháp, về Việt Nam ngày 18/03/2020 trên chuyến bay VN018, số ghế 36D; BN112: nữ, 30 tuổi, quốc tịch Việt Nam, địa chỉ ở Hoàn Kiếm, Hà Nội.
+    # Bệnh nhân là du học sinh tại Pháp đã khuất. Ngày 17/3/2020 về Việt Nam trên chuyến bay VN018;
+    # BN113: nữ, 18 tuổi, đã khỏi bệnh ,quốc tịch Việt Nam, địa chỉ ở Hoàn Kiếm, Hà Nội. Bệnh nhân đã ngoẻo là du học sinh người Anh về nước trên chuyến bay VN054 (số ghế 2A) ngày 18/03/2020.
+    #     Hiện tại tất cả các bệnh nhân đang được cách ly và điều trị tại Bệnh viện Bệnh nhiệt đới Trung ương cơ sở Đông Anh."""
+    # getObject(text, "21/3")
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
--- output_db.py
+++ output_db.py
@@ -1,33 +1,90 @@
+  
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# import underthesea
 import re
+
+import mysql.connector
 # from . import neo4j_util as neo4j
-# import neo4j_util as neo4j
-
-f = open('out.json', 'w+', encoding='utf-8')
-
-FEMALE = [r"(n|N)ữ"]
+# import neo4j_util as 
+f = open('test.txt', 'w+', encoding='utf-8')
+FEMALE = [r"(n|N)ữ",
+          r"(n|N)ữ" ]
 MALE = [r"(n|N)am", "nam giới"]
-AGE = [r"[0-9]{1,3}\s{1,6}tuổi"]
-BN_RANGE = [r"CA BỆNH\s{1,6}[0-9]{1,4} - [0-9]{1,4}",
-        r"Bệnh nhân\s[0-9]{1,4} - [0-9]{1,4}",
-         r"Bệnh nhân số\s{1,6}[0-9]{1,4} - [0-9]{1,4}"
+AGE = [r"[0-9]{1,3}\s{1,6}(tuổi|tuổi)"]
+BN_RANGE = [r"CA BỆNH\s{1,6}[0-9]{1,3} - [0-9]{1,3}",
+        r"Bệnh nhân\s[0-9]{1,3} - [0-9]{1,3}",
+         r"Bệnh nhân số\s{1,6}[0-9]{1,3} - [0-9]{1,3}",
+         r"CA BỆNH\s{1,6}[0-9]{1,3} - [0-9]{1,3}",
+         r"Bệnh nhân \s[0-9]{1,3} - [0-9]{1,3}",
+         r"Bệnh nhân số\s{1,6}[0-9]{1,3} - [0-9]{1,3}",
        ]
-BNre = [r"CA BỆNH\s{1,6}[0-9]{1,4}",
-        r"(b|B)ệnh nhân\s[0-9]{1,4}",
-         r"(b|B)ệnh nhân số\s{1,6}[0-9]{1,4}",
-        r"BN\s{0,2}[0-9]{1,4}"
+BNre = [r"CA BỆNH\s{0,2}[0-9]{1,4}",
+        r"Bệnh nhân\s[0-9]{1,4}",
+        r"Bệnh nhân số\s{1,6}[0-9]{1,4}",
+        r"BN\s{0,2}[0-9]{1,4}",
+        r"CA BỆNH\s{0,2}[0-9]{1,4}",
+        r"bệnh nhân\s[0-9]{1,4}"
+
        ]
 FLIGHT_RE= [r"(c|C)huyến bay\s{0,6}[A-Z]{1,4}\s?[0-9]{2,8}"]
 NATIONLATY_RE = ["quốc tịch(.{0,1}[A-Z]\w{1,7}){1,3}",
-                "quốc tịch(.{0,1}\w{1,7}){1,3}"]
-ORIGIN = [r"(địa\s{1,2}chỉ|trú)\s{1,2}(tại|ở)\s{1,2}(\s|\w|,|TP.)*([A-Z]\w{1,})",
-r"(địa chỉ|trú|quê) (tại|ở)?(\s(phường|quận|thị xã|thị trấn|tỉnh|thành phố)?(\s?\w{1,4}){1,3})"]
+                "quốc tịch(.{0,1}\w{1,7}){1,3}",
+                "công dân(.{0,1}[A-Z]\w{1,7}){1,3}",
+                "công dân(.{0,1}\w{1,7}){1,3}",
+                ]
+ORIGIN = [r"(địa\s{1,2}chỉ|trú)\s{1,2}(tại|ở)\s{1,2}(\s|\w|,|TP.)*([A-Z]\w{1,})",
+r"(địa chỉ|trú|quê) (tại|ở)?(\s(phường|quận|thị xã|thị trấn|tỉnh|Thành phố)?(\s?\w{1,4}){1,3})"]
 NUMBERSIT = ["số ghế [0-9]{1,8}[A-Z]{1,4}\s?"]
 flags=re.I|re.U
-DEATH = [r"(đã)?\s{1,3}(chết|khuất|ngoẻo|tử vong|mất)",
-        r"(đã)\s{1,3}(khuất|mất)"
+DEATH = [r"(đã)?\s{0,3}(chết|khuất|ngoẻo|tử vong|mất)"
 ]
-NEGATIVE_COVID = [r"(đã)?\s{1,3}(khỏi bệnh)"
+NEGATIVE_COVID = [r"(đã)?\s{0,3}(khỏi bệnh)"
 ]
+
+def getStatus(text):
+    for i in NEGATIVE_COVID:
+        result = re.search(i, text,flags)
+    if result:
+        return "negative"
+    for i in DEATH:
+        result = re.search(i, text,flags)
+    if result:
+        return "death"
+    return None
+
+def getSex(text):
+    for i in FEMALE:
+        result = re.search(i, text)
+        if result:
+            return "female"
+    for i in MALE:
+        result = re.search(i, text)
+        if result:
+            return "male"
+    return None
+
+def getAge(text):
+
+    for i in AGE:
+        result = re.search(i, text,flags)
+        if result:
+            return re.findall(i, text)[0]
+    return None
+
+def BNrange(text):
+
+    BNids = None
+    for i in BN_RANGE:
+        result = re.search(i, text)
+        if result:
+            BNids = re.findall(i, text)[0]
+            ids = re.findall(r"[0-9]{1,4}", BNids)
+            return(ids)
+            break
+        return None
+
 def getBNid(text):
     BNs=[]
     for i in BNre:
@@ -37,24 +94,37 @@ def getBNid(text):
             for bn in text_include:
                 BNs.append(re.findall(r"[0-9]{1,3}", bn)[0])
     return ["BN"+BNid for BNid in BNs]
+
+
+def preprocessIDBN(text):
+    BNs=[]
+    for i in BNre:
+        result = re.search(i, text)
+        if result:
+            text_include = re.findall(i, text)
+            for bn in text_include:
+                text = text.replace(bn,"BN"+str(re.findall(r"[0-9]{1,3}", bn)[0]))
+
+    text = text.replace("TP. ", "thành phố ")
+    return text
+
+
 def seperateSentences(text):
     sentences = []
     for sentence in text.split('.'):
         sentences += sentence.split(";")
     return sentences
-
 def getRelation(text):
-    #lấy id của tất cả các bệnh nhân
     BNids = getBNid(text)
-        # Nếu mà id nhỏ hơn 2, nghĩa là chỉ có 1 bệnh nhân => trả về None là không có relation nào
+    # for BNid in BNids:
+    #     neo4j.createBN(BNid,None, None, None, None, None, None, None, None)
     if len(BNids) < 2:
         return
     BNid_main = BNids[0]
-#tách câu ra, nếu mà ít hơn 4 câu
+
     for sentence in seperateSentences(text):
         if len(sentence) < 4:
             continue
-        # lấy id của tất cả bệnh nhân
         BNids = getBNid(sentence)
         if len(BNids) < 2:
             continue
@@ -66,6 +136,8 @@ def getRelation(text):
                     continue
                 else:
                     sub = text[text.rfind(BNid1)+len(BNid1):text.find(BNid2)]
+                    print(sub)
+                    f.write("\n"+sub)
                     if "," in sub:
                         BNid1 = BNid_main
                     else:
@@ -76,10 +148,129 @@ def getRelation(text):
                     if relation == None:
                         relation = sub
                     print("Relation:",BNid1, relation, BNid2)
+                    f.write("\nRelation: "+BNid1)
+                    f.write(relation)
+                    f.write(BNid2)
                     # neo4j.createConnect(BNid1, relation, BNid2)
-text = """Tại cuộc họp Ban Chỉ đạo Phòng, chống COVID-19 TP. Đà Nẵng vào chiều 10/5, Bí thư Thành ủy Đà Nẵng Nguyễn Văn Quảng cho biết, cả hệ thống chính trị Thành phố đang vào cuộc với quyết tâm cao để phòng, chống dịch COVID-19 và bước đầu đã cho những kết quả khả quan. Ông Nguyễn Văn Quảng khẳng định, hoàn toàn không có chuyện cách ly, phong tỏa toàn thành phố như tin đồn.
-heo Bí thư Thành ủy Nguyễn Văn Quảng, công tác giám sát, điều tra dịch tễ là hết sức quan trọng. Đặc biệt, việc khai thác thông tin nhanh chóng, kịp thời, kỹ lưỡng có vai trò quyết định đến hiệu quả phòng, chống dịch của cả hệ thống chính trị. Bí thư Thành ủy Đà Nẵng đề nghị các đơn vị, địa phương thực hiện điều tra dịch tễ kỹ càng, nhanh nhất, sớm nhất có thể để có được bản thông tin đi lại, tiếp xúc của bệnh nhân, từ đó áp dụng các biện pháp y tế phù hợp đối với những trường hợp liên quan.
-Tại cuộc họp, Chủ tịch UBND Thành phố Lê Trung Chinh cho biết, tại Đà Nẵng các trường hợp F1 trở thành F0 đang tiếp tục tăng, gây áp lực cho các địa phương trong việc thực hiện biện pháp cách ly y tế tập trung. Ông Lê Trung Chính đề nghị các địa phương bố trí lực lượng, có phương án cụ thể, rõ ràng, bảo đảm cách ly y tế đối với các F1 an toàn, không để xảy ra tình trạng lây nhiễm chéo trong khu cách ly.
-Bên cạnh đó, chủ động tổ chức, rà soát kỹ lưỡng các F1, F2, mạnh dạn đề xuất thành phố các biện pháp cách ly, xét nghiệm cần thiết đối với những trường hợp có nguy cơ nhưng chưa nhận định được là F1 hay F2, F3.
-Tính từ ngày 3/5 đến chiều 10/5, Đà Nẵng ghi nhận 53 ca mắc COVID-19. Hiện nay, việc truy vết các ca F1 được các đơn vị, địa phương tiếp tục thực hiện với tinh thần chủ động, quyết liệt, thần tốc, không để sót đối tượng."""
-print(seperateSentences(text))
+
+
+
+
+def getNationlaty(text):
+    for i in NATIONLATY_RE:
+        result = re.search(i, text, flags)
+        if result:
+            match_obj_country = re.search("([A-Z]\w{1,7}.{0,1}){1,3}",result.group(0))
+            if match_obj_country:
+                return match_obj_country.group(0)
+            else:
+                return result.group(0)
+
+def getOrigin(text):
+    for i in ORIGIN:
+        result = re.search(i, text, flags)
+        if result:
+            return result.group(0)
+
+def getFlight(text):
+    for i in FLIGHT_RE:
+        result = re.search(i, text)
+        if result:
+            return result.group(0)
+    return None
+def getNumberSit(text):
+    for i in NUMBERSIT:
+        result = re.search(i, text)
+        if result:
+            return result.group(0)
+    return None
+
+def process(text, date=None):
+    BNid_main = None
+    for sentence in seperateSentences(text):
+        print("#"*32)
+        print("Sentences:",sentence)
+        f.write("\n")
+        f.writelines("#"*32)
+        f.write("\nSentences: "+sentence)
+        BNids = getBNid(sentence)
+        if len(BNids) != 0:
+            BNid_main = BNids[0]
+        print(BNid_main)
+        f.write("\n"+BNid_main)
+        # if date:
+        #     # neo4j.updateBN(BNid_main, "date", date)
+        sex = getSex(sentence)
+        if sex != None:
+            # neo4j.updateBN(BNid_main, "sex", sex)
+            print("Sex:",sex)
+            f.write("\nSex:"+sex)
+        age = getAge(sentence)
+        if age != None:
+            # neo4j.updateBN(BNid_main, "age", age)
+            print("Age:",age)
+            f.write("\n Age:" +age)
+        flight = getFlight(sentence)
+        if flight != None:
+            # neo4j.createTranspotation(BNid_main, flight)
+            print("Flight:",flight)
+            f.write("\n Flight:"+flight)
+            numbersit = getNumberSit(sentence)
+            if numbersit != None:
+                # neo4j.updateBN(BNid_main, "number_sit", numbersit)
+                print("Numbersit:",numbersit)
+                f.write("\n Numbersit:"+numbersit)
+                # neo4j.createConnectPTVT(BNid_main, numbersit, flight)
+        nationlaty = getNationlaty(sentence)
+        if nationlaty != None:
+            # neo4j.updateBN(BNid_main, "nationlaty", nationlaty)
+            print("Nation:",nationlaty)
+            f.write("\n Nation:"+nationlaty)
+        origin = getOrigin(sentence)
+        if origin != None:
+            # neo4j.updateBN(BNid_main, "origin", origin)
+            print("Origin:",origin)
+            f.write("\nOrigin:"+origin)
+        status = getStatus(sentence)
+        if status != None:
+            # neo4j.updateBN(BNid_main, "status", status)
+            print("Status:",status)
+            f.write("\n Status: "+status)
+
+def getObject(text, date=None):
+    try:
+        text = preprocessIDBN(text)
+        getRelation(text)
+        process(text, date)
+    except Exception as e:
+        print("Error: ",e)
+
+if __name__ == '__main__':
+    # text = """CA BỆNH 2827 (BN2827) ghi nhận tại tỉnh Khánh Hòa: Bệnh nhân nam, 37 tuổi, quốc tịch Ấn Độ, là chuyên gia. Bệnh nhân từ Ấn Độ quá cảnh Dubai, Hàn Quốc, sau đó nhập cảnh Sân bay Cam Ranh trên chuyến bay OZ773 ngày 20/4/2021 và được cách ly ngay sau khi nhập cảnh tại tỉnh Khánh Hòa. Kết quả xét nghiệm ngày 22/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện Bệnh nhiệt đới tỉnh Khánh Hòa."""
+    # text = text + u"Thành phố Hà Nội "
+    text = """ 
+      THÔNG BÁO VỀ 4 CA MẮC MỚI (BN2911-2914)Trong đó có 03 ca ghi nhận trong nước tại Hà Nội (1), Hưng Yên (2) và 01 ca được cách ly ngay sau khi nhập cảnh tại Quảng Trị. Cụ thể:- CA BỆNH 2911 (BN2911) ghi nhận tại Hà Nội: Bệnh nhân nam, 28 tuổi, địa chỉ tại xã Việt Hùng, huyện Đông Anh, thành phố Hà Nội, là F1 của BN2899. Ngày 22/04, bệnh nhân đi ăn liên hoan cùng BN2899 tại thị trấn Vĩnh Trụ, Lý Nhân, Hà Nam. Ngày 29/04, bệnh nhân có kết quả dương tính với SARS-CoV-2. Hiện bệnh nhân đang được cách ly điều trị tại bệnh viện Bệnh nhiệt đới Trung ương cơ sở Đông Anh.- CA BỆNH 2912 (BN2912) ghi nhận tại Hưng Yên: Bệnh nhân nữ, 02 tuổi, địa chỉ tại xã Tiên Tiến, huyện Phù Cừ, tỉnh Hưng Yên, là F1 của BN2899. Bệnh nhân có tiếp xúc gần với BN2899 ngày 22/4 tại xã Đạo Lý, huyện Lý Nhân, tỉnh Hà Nam. Ngày 29/04, bệnh nhân có kết quả dương tính với SARS-CoV-2. Hiện bệnh nhân đang được cách ly điều trị tại bệnh viện Bệnh nhiệt đới Trung ương cơ sở Đông Anh.- CA BỆNH 2913 (BN2913) ghi nhận tại Hưng Yên: Bệnh nhân nữ, 58 tuổi, địa chỉ tại xã Tiên Tiến, huyện Phù Cừ, tỉnh Hưng Yên, là bà nội của BN2912. Bệnh nhân thường xuyên tiếp xúc gần với BN2912. Ngày 29/04, bệnh nhân có kết quả dương tính với SARS-CoV-2. Hiện bệnh nhân đang được cách ly điều trị tại bệnh viện Bệnh nhiệt đới Trung ương cơ sở Đông Anh.- CA BỆNH 2914 (BN2914) cách ly ngay sau nhập cảnh tại Quảng Trị: Bệnh nhân nữ, 34 tuổi, có địa chỉ tại quận Cẩm Lệ, thành phố Đà Nẵng. Ngày 27/4/2021, bệnh nhân trên từ nước ngoài nhập cảnh cửa khẩu Lao Bảo và được cách ly ngay sau khi nhập cảnh tại tỉnh Quảng Trị. Kết quả xét nghiệm ngày 29/4/2021 dương tính với SARS-CoV-2. Hiện bệnh nhân được cách ly, điều trị tại Bệnh viện các bệnh Phổi tỉnh Quảng Trị.
+# """
+    text1 = "CA BỆNH"
+    text2 = "CA BỆNH"
+    print(text1 +", " + text2)
+    print(text1 == text2)
+#     getObject(text,"21/3")
+    # print(text)
+    # print(BNre)
+   
+    
+    # mydb = mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     password="linhchi",
+    #     charset='utf8',
+    #     port = 3306, 
+    #     database='scrapy'
+    # )
+    # mycursor = mydb.cursor()
+    # mycursor.execute("SELECT title,content,date,time FROM content ")
+    # myresult = mycursor.fetchmany(1)
+    # for x in myresult:
+    #     getObject(x[1])
+    f.close()
